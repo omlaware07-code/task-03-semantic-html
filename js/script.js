cb079d4c21f6de243ff218586601e0bac7035b87
om laware <omlaware07@gmail.com>
2026-09-16 22:09:59 +0530

    Create accessible semantic HTML5 dashboard

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+
 const openModalButton = document.getElementById("openModal");
+
 const closeModalButton = document.getElementById("closeModal");
+
 const reportModal = document.getElementById("reportModal");
 
 openModalButton.addEventListener("click", () => {
+
   reportModal.showModal();
+
 });
 
 closeModalButton.addEventListener("click", () => {
+
   reportModal.close();
+
 });
+
